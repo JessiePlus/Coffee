71295680881b440df3a166df2850583ea69d1e05
DingLin <dinglin1991@yeah.net>
2016-09-07 14:54:24 +0800

    初始化项目

---
+++ Coffee/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Coffee
+//
+//  Created by dinglin on 16/9/7.
+//  Copyright © 2016年 dinglin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
