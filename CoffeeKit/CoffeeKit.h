e897ef658e170c3f43a33ecf24e7821ac7ed257e
DingLin <dinglin1991@yeah.net>
2016-09-08 11:13:01 +0800

    修改界面

---
+++ CoffeeKit/CoffeeKit.h
@@ -0,0 +1,19 @@
+//
+//  CoffeeKit.h
+//  CoffeeKit
+//
+//  Created by dinglin on 16/9/7.
+//  Copyright © 2016年 dinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoffeeKit.
+FOUNDATION_EXPORT double CoffeeKitVersionNumber;
+
+//! Project version string for CoffeeKit.
+FOUNDATION_EXPORT const unsigned char CoffeeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoffeeKit/PublicHeader.h>
+
+
